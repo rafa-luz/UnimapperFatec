e3c6c813c5b0d62e0a489b884cd98867bf9bacbb
rafa-luz <100418673+rafa-luz@users.noreply.github.com>
2023-10-23 10:11:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function changeSlide(slideNumber) {
-    window.location.href = 'slide' + slideNumber + '.html';
-  }
-  
+document.addEventListener("DOMContentLoaded", function() {
+    var loading = document.querySelector(".loading");
+
+    // Mostrar o loading por 7 segundos
+    setTimeout(function() {
+        // Redirecionar para apresentacao1.html após 7 segundos
+        window.location = "./pages/apresentacao1.html"
+    }, 7000); // 7000 milissegundos = 7 segundos
+});
